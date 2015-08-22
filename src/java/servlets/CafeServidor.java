c1bb99d143377fcb983cf6c74965fa3a2580dc10
slk <slk@DESKTOP-AJKPOGV>
2015-08-22 15:44:35 -0300

    3º comit -melhorar o método doPostO para chamar o modelo para oferecer o conselho (EspecialistaEmCafe.java)

--- src/java/servlets/CafeServidor.java
+++ src/java/servlets/CafeServidor.java
@@ -1,9 +1,10 @@
 package servlets;
-
 import java.io.*;
+import java.util.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import model.EspecialistaEmCafe;
 
 /**
  *
@@ -11,17 +12,35 @@ import javax.servlet.http.*;
  */
 public class CafeServidor extends HttpServlet {
 
-  
-   
-   
+    
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        
+//        Versão 2
+        String tipo = request.getParameter("tipo");
+        EspecialistaEmCafe espc = new EspecialistaEmCafe();
+        List result = espc.getMarcas(tipo);
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("Sua opção foi processada com sucesso");
-        String tipo = request.getParameter("tipo");
-        out.println("<br> Conselho sobre o café &nbsp" + tipo);
+        out.println("Selecione o conselho para o café");
+        
+        Iterator it = result.iterator();
+        while (it.hasNext()) {
+           out.print("<br> Especialista: " + it.next());
+            
+        }
+                
+//        versão 1
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("Sua opção foi processada com sucesso");
+//        String tipo = request.getParameter("tipo");
+//        out.println("<br> Conselho sobre o café &nbsp" + tipo);
+        
+        
         
     }
 }
